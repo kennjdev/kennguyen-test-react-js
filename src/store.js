aa7a917a7840c8f02b4960a8ed87027155ecfcfb
kennj <kennj.dev@gmail.com>
2022-04-05 13:24:33 +0700

    clean code

--- src/store.js
+++ src/store.js
@@ -1,26 +1,14 @@
-import {applyMiddleware, compose, createStore} from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from 'redux-thunk';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import {persistReducer} from "redux-persist";
-import storage from 'redux-persist/lib/storage';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from "./reducers";
-const persistConfig = {
-      key: 'root',
-      storage: storage,
-      stateReconciler: autoMergeLevel2,
-      // whitelist: ['auth','message']
-};
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const configStore = () => {
       const store = createStore(
-            persistedReducer,
-            composeEnhancers(applyMiddleware(thunk)),
+            rootReducer,
+            composeWithDevTools(
+                  applyMiddleware(thunk),
+            )
       );
       return store;
 };
-
 export default configStore;
